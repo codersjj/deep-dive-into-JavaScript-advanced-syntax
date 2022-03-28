4e80e3030136d9336bc907a1098c7edd99c2b386
Shane678 <shanewestlife@outlook.com>
2022-03-28 22:38:50 +0800

    feat: call 函数的实现之最终结果的返回

--- 实现 call、apply、bind/call 函数的实现.js
+++ 实现 call、apply、bind/call 函数的实现.js
@@ -1,17 +1,18 @@
 // 给所有的函数添加一个 jjCall 的方法
-// 使用 ES6 新增的 rest parameters（剩余参数）收集其余传入的参数
 Function.prototype.jjCall = function (thisArg, ...argArray) {
   // 1. 获取需要被执行的函数
   const fn = this
 
-  // 将 thisArg 转换为对象类型（防止它是非对象类型时调用方法出现报错）
+  // 2. 将 thisArg 转换为对象类型（防止它是非对象类型时调用方法出现报错）
   thisArg = thisArg ? Object(thisArg) : globalThis
 
-  // 2. 调用需要被执行的函数
+  // 3. 调用需要被执行的函数
   thisArg.fn = fn
-  // 使用 ES6 新增的 spread arguments（展开参数）展开参数数组，挨个进行传值
-  thisArg.fn(...argArray)
+  const res = thisArg.fn(...argArray)
   delete thisArg.fn
+
+  // 4. 将最终的结果返回出去
+  return res
 }
 function foo() {
   console.log('foo 函数被调用了~', this, this === globalThis);
@@ -19,6 +20,7 @@ function foo() {
 
 function sum(num1, num2) {
   console.log('sum 函数被调用了~', this, num1, num2);
+  return num1 + num2
 }
 
 // JavaScript 的函数的 call 方法
@@ -27,7 +29,8 @@ foo.call(undefined)
 foo.call(null)
 foo.call({ name: 'zhj' })
 foo.call(123)
-sum.call('哈哈哈', 2000, 22)
+const callRes = sum.call('哈哈哈', 2000, 22)
+console.log('调用 call 方法的结果：', callRes);
 
 // 自己实现的函数的 call 方法
 foo.jjCall()
@@ -35,4 +38,5 @@ foo.jjCall(undefined)
 foo.jjCall(null)
 foo.jjCall({ name: 'zhj' })
 foo.jjCall(123)
-sum.jjCall('哈哈哈', 2000, 22)
+const jjCallRes = sum.jjCall('哈哈哈', 2000, 22)
+console.log('调用 jjCall 方法的结果：', jjCallRes);
