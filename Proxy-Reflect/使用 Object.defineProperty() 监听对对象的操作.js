41256969f7d2c3018a1708939b82e5ad7c3095de
Shane678 <shanewestlife@outlook.com>
2022-04-05 21:18:04 +0800

    feat: 使用 Object.defineProperty() 监听对对象的操作之监听对象的所有属性

--- Proxy-Reflect/使用 Object.defineProperty() 监听对对象的操作.js
+++ Proxy-Reflect/使用 Object.defineProperty() 监听对对象的操作.js
@@ -1,20 +1,27 @@
 const obj = {
-  name: 'zhj'
+  name: 'zhj',
+  age: 20
 }
 
-let value = obj.name
+Object.keys(obj).forEach(key => {
+  let value = obj[key]
 
-Object.defineProperty(obj, 'name', {
-  get() {
-    console.log('监听到 obj 对象的 name 属性被访问了~');
-    return value
-  },
-  set(newValue) {
-    console.log('监听到 obj 对象的 name 属性被设置值了~');
-    value = newValue
-  }
+  Object.defineProperty(obj, key, {
+    get() {
+      console.log(`监听到 obj 对象的 ${key} 属性被访问了~`);
+      return value
+    },
+    set(newValue) {
+      console.log(`监听到 obj 对象的 ${key} 属性被设置值了~`);
+      value = newValue
+    }
+  })
 })
 
 console.log(obj.name);
 obj.name = 'wy'
 console.log(obj.name);
+
+console.log(obj.age);
+obj.age = 22
+console.log(obj.age);
