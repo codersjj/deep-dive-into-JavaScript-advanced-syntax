90ff564d4e90be836219c191d93c51d1f9fbbcbb
Shane678 <shanewestlife@outlook.com>
2022-04-06 21:48:51 +0800

    refactor: 直接使用匿名函数，将其变成响应式函数

--- 响应式原理的实现/02_响应式函数的封装.js
+++ 响应式原理的实现/02_响应式函数的封装.js
@@ -9,29 +9,22 @@ const obj = {
   age: 20
 }
 
-// 用函数封装要自动执行的这段代码
-function foo() {
+// 调用 watchFn 函数，将目标函数变成响应式函数
+watchFn(function() {
   const newName = obj.name
   console.log('哈哈哈', obj.name);
-}
-
-function baz() {
+})
+watchFn(function() {
   console.log('baz 函数被调用了~');
-}
+})
 
 function bar() {
   console.log('普通的其它函数，不需要响应式');
 }
 
-// 调用 watchFn 函数，将目标函数变成响应式函数
-watchFn(foo)
-watchFn(baz)
-
-// obj 对象 name 属性的值改变了，自动去执行 foo 函数
+// obj 对象 name 属性的值改变了，自动去执行相应的响应式函数
 obj.name = 'wy'
 
-// 单独调用函数
-// foo()
 // 遍历相应的响应式函数数组，挨个调用函数
 reactiveFns.forEach(reactiveFn => {
   reactiveFn()
