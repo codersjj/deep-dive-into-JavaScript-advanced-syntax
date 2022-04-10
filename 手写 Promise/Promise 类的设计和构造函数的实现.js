9ce25edb97784f846ea8703b77e092f80f3801d2
Shane678 <shanewestlife@outlook.com>
2022-04-10 22:25:32 +0800

    feat(手写 Promise): 考虑 resolve() 和 reject() 中传入参数的情形

--- 手写 Promise/Promise 类的设计和构造函数的实现.js
+++ 手写 Promise/Promise 类的设计和构造函数的实现.js
@@ -8,16 +8,21 @@ class JJPromise {
   constructor(executor) {
     this.status = PROMISE_STATUS_PENDING
 
-    const resolve = () => {
+    this.value = undefined
+    this.reason = undefined
+
+    const resolve = (value) => {
       if (this.status === PROMISE_STATUS_PENDING) {
         this.status = PROMISE_STATUS_FULFILLED
+        this.value = value
         console.log('resolve 被调用了~');
       }
     }
 
-    const reject = () => {
+    const reject = (reason) => {
       if (this.status === PROMISE_STATUS_PENDING) {
         this.status = PROMISE_STATUS_REJECTED
+        this.reason = reason
         console.log('reject 被调用了~');
       }
     }
@@ -28,6 +33,12 @@ class JJPromise {
 
 const promise = new JJPromise((resolve, reject) => {
   console.log('executor 执行了~', '状态：pending');
-  resolve()
-  reject()
+  resolve(111)
+  reject(222)
 })
+
+// promise.then(res => {
+//   console.log('res:', res);
+// }, err => {
+//   console.log('err:', err);
+// })
