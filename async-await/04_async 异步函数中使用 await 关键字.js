75061e5bd3e40d3016681e0ca317a07815d9a8ea
codersjj <shanewestlife@outlook.com>
2022-05-02 22:29:39 +0800

    feat(async/await): async 异步函数中使用 await_await 后面跟其它值

--- async-await/04_async 异步函数中使用 await 关键字.js
+++ async-await/04_async 异步函数中使用 await 关键字.js
@@ -4,23 +4,44 @@
 // }
 
 /* 2. await 后面通常会跟一个表达式，这个表达式会返回一个 Promise，await 会等待这个 Promise 的状态变成 fulfilled，之后继续执行异步函数 */
+// async function foo() {
+//   // await 表达式
+//   const res = await requestData() // await 的结果是表达式返回的 Promise resolve 的结果
+//   // 在 await 后面的表达式返回的 Promise resolve 之前，后面的代码都不会执行，
+//   // 在 await 后面的表达式返回的 Promise resolve 之后，后面的代码才会执行，所以可以把后面的代码看成是在 Promise 的 then 里面执行的代码
+//   console.log('后面的代码', res);
+
+//   const res2 = await requestData()
+//   console.log('res2:', res2);
+// }
+
+// function requestData() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(123)
+//     }, 2000)
+//   })
+// }
+
+// foo()
+
 async function foo() {
-  // await 表达式
-  const res = await requestData() // await 的结果是表达式返回的 Promise resolve 的结果
-  // 在 await 后面的表达式返回的 Promise resolve 之前，后面的代码都不会执行，
-  // 在 await 后面的表达式返回的 Promise resolve 之后，后面的代码才会执行，所以可以把后面的代码看成是在 Promise 的 then 里面执行的代码
-  console.log('后面的代码', res);
+  // await 后面跟普通的值时，会立即直接返回这个值
+  // const res = await 666
 
-  const res2 = await requestData()
-  console.log('res2:', res2);
-}
+  // await 后面也可以跟实现了 thenable 接口的对象
+  // const res = await {
+  //   then: function(resolve, reject) {
+  //     resolve(666)
+  //   }
+  // }
 
-function requestData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(123)
-    }, 2000)
+  // await 后面还可以跟一个 Promise
+  const res = await new Promise((resolve, reject) => {
+    resolve(666)
   })
+
+  console.log('res:', res);
 }
 
 foo()
