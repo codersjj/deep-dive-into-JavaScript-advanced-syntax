ecf6f03f89586a78f4cad456987bf04d624ac4f6
codersjj <shanewestlife@outlook.com>
2022-05-02 15:35:05 +0800

    feat: 异步代码的处理方案_使用回调函数

---
+++ 迭代器-生成器/14_异步代码的处理方案.js
@@ -0,0 +1,18 @@
+/* 1. 使用回调函数 */
+function requestData(url, successCallback, failureCallback) {
+  setTimeout(() => {
+    if (url === 'coderzhj') {
+      const res = 111
+      successCallback(res)
+    } else {
+      const err = 'url 错误'
+      failureCallback(err)
+    }
+  }, 2000)
+}
+
+requestData('coderzhj', (res) => {
+  console.log('成功拿到结果：', res);
+}, (err) => {
+  console.log('失败，错误信息：', err);
+})
