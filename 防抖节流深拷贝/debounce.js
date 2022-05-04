4ccb5fa178f6350de8f5301e8a5de5c164524d2e
codersjj <shanewestlife@outlook.com>
2022-05-04 11:41:33 +0800

    feat(debounce): 防抖函数的基本实现_考虑 this 和参数

--- 防抖节流深拷贝/debounce.js
+++ 防抖节流深拷贝/debounce.js
@@ -1,18 +1,12 @@
 function debounce(fn, delay) {
-  // 定义一个定时器，保存上一次的定时器
   let timer = null
 
-  // 真正执行的函数
-  const _debounce = function() {
-    // 每次触发事件前先取消上一次定时器（第一次的时候 timer 还没有，所以最好用 if 判断一下）
+  const _debounce = function(...args) {
     if (timer) clearTimeout(timer)
-    // 延迟执行
     timer = setTimeout(() => {
-      // 外部传入的真正要执行的函数
-      fn()
+      fn.apply(this, args)
     }, delay)
   }
 
-  // 返回一个新的（做了防抖的）函数
   return _debounce
 }
