d9e6d1a3a6599b6d74406febca4d6382b16dd64a
codersjj <shanewestlife@outlook.com>
2022-05-04 13:54:09 +0800

    feat(debounce): 防抖函数添加是否立即执行功能

--- 防抖节流深拷贝/debounce.js
+++ 防抖节流深拷贝/debounce.js
@@ -1,11 +1,21 @@
-function debounce(fn, delay) {
+function debounce(fn, delay, immediate = false) {
   let timer = null
+  // 是否被调用过，用来实现在中途停掉后再次触发事件时，响应函数还能立即执行
+  let isInvoke = false
 
   const _debounce = function(...args) {
     if (timer) clearTimeout(timer)
-    timer = setTimeout(() => {
+
+    // 判断是否要立即执行
+    if (immediate && !isInvoke) {
       fn.apply(this, args)
-    }, delay)
+      isInvoke = true
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+        isInvoke = false
+      }, delay)
+    }
   }
 
   return _debounce
