e7ef301da8b8b0394a9461ae250cae5a2e85af74
codersjj <shanewestlife@outlook.com>
2022-05-05 20:05:01 +0800

    feat(throttle): 节流函数添加获取返回值的功能_通过 Promise 实现

--- 防抖节流深拷贝/throttle.js
+++ 防抖节流深拷贝/throttle.js
@@ -7,39 +7,43 @@ function throttle(fn, interval, options = { leading: true, trailing: false }) {
 
   // 事件触发时，真正执行的函数
   const _throttle = function(...args) {
-    // 获取当前事件触发时的时间
-    const currentTime = Date.now()
+    return new Promise((resolve, reject) => {
+      // 获取当前事件触发时的时间
+      const currentTime = Date.now()
 
-    if (!lastTime && !leading) lastTime = currentTime
+      if (!lastTime && !leading) lastTime = currentTime
 
-    // 计算还剩余多长时间需要去执行响应函数
-    const remainTime = interval - (currentTime - lastTime)
-    if (remainTime <= 0) {
-      if (timer) {
-        clearTimeout(timer)
-        timer = null
-      }
-      // 剩余时间小于等于 0 时真正触发响应函数
-      const result = fn.apply(this, args)
-      if (resultCallback) {
-        resultCallback(result)
-      }
-      // 更新最近一次响应函数执行的时间点，以便开始下一个计时周期
-      lastTime = currentTime
-
-      return
-    }
-
-    if (trailing && !timer) {
-      timer = setTimeout(() => {
+      // 计算还剩余多长时间需要去执行响应函数
+      const remainTime = interval - (currentTime - lastTime)
+      if (remainTime <= 0) {
+        if (timer) {
+          clearTimeout(timer)
+          timer = null
+        }
+        // 剩余时间小于等于 0 时真正触发响应函数
         const result = fn.apply(this, args)
         if (resultCallback) {
           resultCallback(result)
         }
-        timer = null
-        lastTime = !leading ? 0 : new Date().getTime()
-      }, remainTime)
-    }
+        resolve(result)
+        // 更新最近一次响应函数执行的时间点，以便开始下一个计时周期
+        lastTime = currentTime
+
+        return
+      }
+
+      if (trailing && !timer) {
+        timer = setTimeout(() => {
+          const result = fn.apply(this, args)
+          if (resultCallback) {
+            resultCallback(result)
+          }
+          resolve(result)
+          timer = null
+          lastTime = !leading ? 0 : new Date().getTime()
+        }, remainTime)
+      }
+    })
   }
 
   _throttle.cancel = function() {
