08650db0f9a50b206ccbd414634342ae5141ea37
codersjj <shanewestlife@outlook.com>
2022-05-05 19:31:11 +0800

    feat(throttle): 节流函数添加 trailing 功能

--- 防抖节流深拷贝/throttle.js
+++ 防抖节流深拷贝/throttle.js
@@ -3,6 +3,7 @@ function throttle(fn, interval, options = { leading: true, trailing: false }) {
 
   // 记录上一次的开始时间（最近一次响应函数执行的时间点）
   let lastTime = 0
+  let timer = null
 
   // 事件触发时，真正执行的函数
   const _throttle = function() {
@@ -14,10 +15,24 @@ function throttle(fn, interval, options = { leading: true, trailing: false }) {
     // 计算还剩余多长时间需要去执行响应函数
     const remainTime = interval - (currentTime - lastTime)
     if (remainTime <= 0) {
+      if (timer) {
+        clearTimeout(timer)
+        timer = null
+      }
       // 剩余时间小于等于 0 时真正触发响应函数
       fn()
       // 更新最近一次响应函数执行的时间点，以便开始下一个计时周期
       lastTime = currentTime
+
+      return
+    }
+
+    if (trailing && !timer) {
+      timer = setTimeout(() => {
+        fn()
+        timer = null
+        lastTime = !leading ? 0 : new Date().getTime()
+      }, remainTime)
     }
   }
 
