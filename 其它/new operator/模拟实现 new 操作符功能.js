880413cf4875f6498250a2f88966236fe23f7d19
codersjj <shanewestlife@outlook.com>
2022-05-15 18:30:03 +0800

    refactor(new operator): 模拟实现 new 操作符的功能_使用 ES6 语法（rest parameters、const）进行改写

--- 其它/new operator/模拟实现 new 操作符功能.js
+++ 其它/new operator/模拟实现 new 操作符功能.js
@@ -1,26 +1,22 @@
 // 手写一个函数，模拟实现 new 操作符的功能
 
-function useNewOperator() {
-  var constructor = arguments[0]
-
+function useNewOperator(constructor, ...args) {
   if (typeof constructor !== 'function') {
     throw new TypeError('the first argument to useNewOperator function must be a function')
   }
 
-  var args = [].slice.call(arguments, 1)
-
   // 1. 在内存中创建一个新对象
-  var obj = {}
+  const obj = {}
 
   // 2. 这个新对象内部的 [[Prototype]] 指针被赋值为构造函数（constructor）的 prototype 属性
   // 不推荐使用 Object.prototype.__proto__，更推荐使用 Object.getPrototypeOf/Reflect.getPrototypeOf 和Object.setPrototypeOf/Reflect.setPrototypeOf（参考链接：https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Object/proto）
   Object.setPrototypeOf(obj, constructor.prototype)
   // 或者使用 Object.create() 直接指定原型创建新对象
-  // var obj = Object.create(constructor.prototype)
+  // const obj = Object.create(constructor.prototype)
 
   // 3. 构造函数内部的 this 被赋值为这个新对象（即 this 指向新对象）
   // 4. 执行构造函数内部的代码（给新对象添加属性）
-  var res = constructor.apply(obj, args)
+  const res = constructor.apply(obj, args)
 
   // 5. 如果构造函数返回非空对象，则返回该对象；否则，返回刚才创建的新对象
   if (res != null && (typeof res === 'object' || typeof res === 'function')) {
