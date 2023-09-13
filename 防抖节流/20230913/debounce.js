40cbd209c1218aea95634456c804ed6e42a588a9
codersjj <shanewestlife@outlook.com>
2023-09-13 12:46:20 +0800

    chore: practice-debounce-v3-添加立即执行功能

--- 防抖节流/20230913/debounce.js
+++ 防抖节流/20230913/debounce.js
@@ -1,11 +1,27 @@
-function debounce(fn, wait) {
+function now() {
+  // Date.now() 是 ES5 新增的，在此之前不存在该函数
+  if (Date.now) return Date.now()
+  return new Date().getTime()
+}
+function debounce(fn, wait, immediate = false) {
   let timer = null
+  let isInvoked = false
+  let previous = 0
+  let passed = 0
   function debounced(...args) {
     if (timer) clearTimeout(timer)
-    timer = setTimeout(() => {
+    passed = now() - previous
+    previous = now()
+    if (immediate && (!isInvoked || passed >= wait)) {
       fn.apply(this, args)
-      timer = null
-    }, wait)
+      isInvoked = true
+    } else {
+      timer = setTimeout(() => {
+        fn.apply(this, args)
+        timer = null
+        isInvoked = false
+      }, wait)
+    }
   }
 
   return debounced
