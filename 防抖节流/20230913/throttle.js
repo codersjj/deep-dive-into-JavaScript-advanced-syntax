3cc79bdc820ee13a38658504ac0146ac526947d8
codersjj <shanewestlife@outlook.com>
2023-09-13 13:06:46 +0800

    chore: practice-throttle-v2-添加首次 leading 可取消功能

--- 防抖节流/20230913/throttle.js
+++ 防抖节流/20230913/throttle.js
@@ -4,10 +4,12 @@ function now() {
   // Date.prototype.getTime() 是 ES1 开始就有的
   return new Date().getTime()
 }
-function throttle(fn, wait) {
+function throttle(fn, wait, option = { leading: true, trailing: true }) {
+  const { leading, trailing } = option
   let previous = 0
   function throttled() {
     const _now = now()
+    if (!leading && !previous) previous = _now
     const remaining = wait - (_now - previous)
     if (remaining <= 0) {
       fn()
