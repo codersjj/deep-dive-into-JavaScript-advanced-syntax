164275d4333c1628b1998ffe6c00442d21812c67
codersjj <shanewestlife@outlook.com>
2023-09-14 10:27:44 +0800

    chore: practice-深拷贝-v4-考虑 Symbol

--- 深拷贝/20230914/deepCopy.js
+++ 深拷贝/20230914/deepCopy.js
@@ -3,12 +3,16 @@ function isObject(value) {
   return (type === 'object' && value !== null) || type === 'function'
 }
 function deepCopy(value) {
+  if (typeof value === 'symbol') return Symbol(value.description)
   if (!isObject(value)) return value
 
   if (typeof value === 'function') return value
 
   const newValue = Array.isArray(value) ? [] : {}
   Object.keys(value).forEach(key => {
+    newValue[key] = deepCopy(value[key])
+  })
+  Object.getOwnPropertySymbols(value).forEach(key => {
     newValue[key] = deepCopy(value[key])
   })
   return newValue
