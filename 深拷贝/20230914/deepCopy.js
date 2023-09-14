a816bec94c9438721cfbd741ad72fb880d5c949b
codersjj <shanewestlife@outlook.com>
2023-09-14 11:24:20 +0800

    chore: practice-深拷贝-v6-解决循环引用问题-优化1-以参数方式管理 map

--- 深拷贝/20230914/deepCopy.js
+++ 深拷贝/20230914/deepCopy.js
@@ -2,8 +2,7 @@ function isObject(value) {
   const type = typeof value
   return (type === 'object' && value !== null) || type === 'function'
 }
-const map = new Map()
-function deepCopy(value) {
+function deepCopy(value, map = new Map()) {
   if (typeof value === 'symbol') return Symbol(value.description)
   if (!isObject(value)) return value
 
@@ -11,14 +10,14 @@ function deepCopy(value) {
   if (Object.prototype.toString.call(value) === '[object Map]') {
     const newMap = new Map()
     value.forEach((item, key) => {
-      newMap.set(key, deepCopy(item))
+      newMap.set(key, deepCopy(item, map))
     })
     return newMap
   }
   if (Object.prototype.toString.call(value) === '[object Set]') {
     const newSet = new Set()
     value.forEach((item) => {
-      newSet.add(deepCopy(item))
+      newSet.add(deepCopy(item, map))
     })
     return newSet
   }
@@ -28,10 +27,10 @@ function deepCopy(value) {
   map.set(value, newValue)
 
   Object.keys(value).forEach(key => {
-    newValue[key] = deepCopy(value[key])
+    newValue[key] = deepCopy(value[key], map)
   })
   Object.getOwnPropertySymbols(value).forEach(key => {
-    newValue[key] = deepCopy(value[key])
+    newValue[key] = deepCopy(value[key], map)
   })
   return newValue
 }
